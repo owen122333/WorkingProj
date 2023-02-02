77bb3efa4fa0734fab8312fcfaf087233fc3d849
ovenker4833 <ovenker4833@W10-2C41NL3>
2023-02-02 09:43:41 -0600

    Add project files.

---
+++ WindowsApp4/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        PictureBox1.Visible = True
+        Label1.Visible = True
+        Button1.Visible = False
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
